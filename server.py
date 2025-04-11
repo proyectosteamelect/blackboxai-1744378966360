793353bee87a193804fc032525244c05f2cc589d
proyectosteamelect <danielitogral2@gmail.com>
2025-04-11 08:42:58 -0500

    Add server.py 1744378977221

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import sys
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
+port = 8000
+print(f"Serving at port {port}")
+HTTPServer(('', port), CORSRequestHandler).serve_forever()
